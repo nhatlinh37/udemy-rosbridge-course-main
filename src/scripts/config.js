23fe3878243ef508d046e0cb0a4f290001e85dee
nhatlinh37 <167947342+nhatlinh37@users.noreply.github.com>
2025-01-02 11:35:33 +0700

    Initial commit

---
+++ src/scripts/config.js
@@ -0,0 +1,10 @@
+const Config = {
+  ROSBRIDGE_SERVER_IP: "172.172.5.86",
+  ROSBRIDGE_SERVER_PORT: "9090",
+  RECONNECTION_TIMER: 3000,
+  CMD_VEL_TOPIC: "/cmd_vel",
+  ODOM_TOPIC: "/odom",
+  POSE_TOPIC: "/amcl_pose",
+};
+
+export default Config;
